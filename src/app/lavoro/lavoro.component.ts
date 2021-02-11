a87a7d83c0ed1156a7b5fa99cccc2c5a8b457acc
Laura <laura.a.chelaru@gmail.com>
2021-02-11 12:54:57 +0100

    sistemato pagina lavoro

--- src/app/lavoro/lavoro.component.ts
+++ src/app/lavoro/lavoro.component.ts
@@ -14,31 +14,33 @@ export class LavoroComponent implements OnInit {
 
   lavori : any[] = [
     {
+      id : 1,
       titolo : "Sviluppatore Front-end",
       citta : "Torino",
       descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
     },
     {
+      id: 2,
       titolo : "Sviluppatore Back-end",
       citta : "Bari",
       descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
     },
     {
+      id: 3,
       titolo : "Sviluppatore FullStack",
       citta : "Roma",
       descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
     },
     {
+      id: 4,
       titolo : "Sviluppatore Java",
       citta : "Potenza",
       descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
     }
   ];
 
-  // lavoro : any =     {
-  //   titolo : "Sviluppatore Front-end",
-  //   citta : "Torino",
-  //   descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
-  // };
+  mostraDettagli(id:number) {
+    console.log(id);
+  }
 
 }
