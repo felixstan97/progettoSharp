440ebe1a0389318cf385e184aaefa81c6cf12288
Laura <laura.a.chelaru@gmail.com>
2021-02-11 12:27:57 +0100

    un po' di responsive navbar + sidebar

--- src/app/lavoro/lavoro.component.ts
+++ src/app/lavoro/lavoro.component.ts
@@ -12,4 +12,33 @@ export class LavoroComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  lavori : any[] = [
+    {
+      titolo : "Sviluppatore Front-end",
+      citta : "Torino",
+      descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
+    },
+    {
+      titolo : "Sviluppatore Back-end",
+      citta : "Bari",
+      descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
+    },
+    {
+      titolo : "Sviluppatore FullStack",
+      citta : "Roma",
+      descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
+    },
+    {
+      titolo : "Sviluppatore Java",
+      citta : "Potenza",
+      descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
+    }
+  ];
+
+  // lavoro : any =     {
+  //   titolo : "Sviluppatore Front-end",
+  //   citta : "Torino",
+  //   descrizione : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium magni. Sint reiciendis dolores deleniti maiores deserunt sunt enim velit dolor quasi, vero cumque nemo possimus obcaecati culpa blanditiis ullam."
+  // };
+
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -15,20 +16,39 @@ export class NavbarComponent implements OnInit {
   
   redirectToHome(){
     this.router.navigate(['home']);
+    window.scroll(0,0);
   }
+  
   redirectToLavoro(){
     this.router.navigate(['lavoro']);
+    window.scroll(0,0);
   }
   redirectToCorsi(){
     this.router.navigate(['corso']);
+    window.scroll(0,0);
   }
+
   redirectToContatti(){
     this.router.navigate(['contatti']);
+    window.scroll(0,0);
   }
 
 
   ngOnInit(): void {
   }
+
+  @HostListener('window:scroll', ['$event'])
+
+  
+  onWindowScroll() {
+    let element = document.getElementsByClassName('navbarWrapper')[0];
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-change-color');
+      } else {
+        element.classList.remove('navbar-change-color');
+      }
+    }
+
  
   
 }
